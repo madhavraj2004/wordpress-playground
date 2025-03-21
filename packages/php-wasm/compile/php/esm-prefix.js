b42c72866b6d34200c29aaf74cd8a9689ae74c48
Brandon Payton <brandon@happycode.net>
2025-03-20 21:46:22 -0400

    Move custom dlopen to library file
    This avoids the need to patch and rename a conflicting
    function provided by Emscripten.

--- packages/php-wasm/compile/php/esm-prefix.js
+++ packages/php-wasm/compile/php/esm-prefix.js
@@ -1,11 +1,2 @@
 export function init(RuntimeName, PHPLoader) {
     // The rest of the code comes from the built php.js file and esm-suffix.js
-
-    // TODO: Move this to a library file and stop renaming conflicting
-    // _dlopen_js in the Dockerfile. If we override via library, Emscripten
-    // shouldn't add the conflicting version of the function.
-    const __dlopen_js = function (handle) {
-        var jsflags = { loadAsync: false }
-        return dlopenInternal(handle, jsflags);
-    };
-    __dlopen_js.isAsync = false;
